2c666197354ce0d8f2b36de3f13b25c9d3b9f2ac
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-07-17 01:41:14 -0500

    Submission 1

---
+++ Week_1_App/my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import To_do_list from './to_do_list';
+import Greeting from './to_do_list';
+
+function App() {
+  return (
+    <To_do_list />
+  );
+}
+
+export default App;
---
+++ Week_1_App/my-app/src/to_do_list.js
@@ -0,0 +1,16 @@
+
+
+function To_do_list(){
+    
+    return (
+        <>
+          <h1>List of Todos</h1>
+            <ul><input type='checkbox'></input> goto gym </ul>
+            <ul><input type='checkbox'></input> do laundry </ul>
+            <ul><input type='checkbox'></input> food shopping </ul>
+            <ul><input type='checkbox'></input> do homework </ul>
+        </>
+    );
+}
+
+export default To_do_list
