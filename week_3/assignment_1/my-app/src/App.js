a5755dcf0108b533cb29a94da57e752a5750d414
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-08-01 01:31:29 -0500

    First attempt

--- week_3/assignment_1/my-app/src/App.js
+++ week_3/assignment_1/my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList.js';
+import {todos} from "./STORE.js";
+import {useState} from "react";
 
-function App() {
+export default function App() {
+  const [todosItems, setTodos] = useState(listOfTodos);
+  
+  const completed = id => {
+    const tempItems = [...todosItems];
+    const index = tempItems.findIndex(item => item.id === id);
+    tempItems[index].isComplete = !tempItems[index].isComplete
+    setTodos(tempItems)
+  }
+  
+  const deleteItem = (id) => {
+    const tempItems = [...todosItems];
+    const newItems = tempItems.filter((item) => item.id !== id)
+    setTodos(tempItems)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>ToDo-List</h3>
+        <TodoList todos = {todos} />
     </div>
   );
 }
 
-export default App;
---
+++ week_3/assignment_1/my-app/src/TodoList.js
@@ -0,0 +1,12 @@
+import Todo from './Todo';
+
+export default function TodoList( {item, completed, deleteItem}) {
+  const todosList = item.map((item) => {
+    return <Todo key = {item.id} item={item} completed={completed} deleteItem={deleteItem} />
+  });
+  return (
+    <>
+      <ul>{todosList}</ul>
+    </>
+    )
+}
