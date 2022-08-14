f0400bdee7f4d07151eefe4f5fc91066b710b2a4
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-08-14 11:43:20 -0500

    General CSS layout

--- week_5/assignment_1/src/App.js
+++ week_5/assignment_1/src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <div className="searchBox">
+        Enter a City: (Ex: Phoenix, AZ) <br/>
+        <input id='searchBox'type="text" placeholder="Phoenix, AZ"></input> <br/>
+        <button id='getWeather'>Get Weather</button>
+      </div>
+      <div className="searchResults">
+        <h3 className= 'resultCityName'>Weather For "city"</h3>
+        uknown data value
+      </div>
     </div>
   );
 }
