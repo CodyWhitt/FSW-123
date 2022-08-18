4588629d6c762dfa11efde6357e7262f7f2f19b0
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-08-17 22:50:20 -0500

    small change

--- week_4/assignment_1/src/TodoList.js
+++ week_4/assignment_1/src/TodoList.js
@@ -2,10 +2,9 @@ import Todo from './Todo';
 
 export default function TodoList({item, completed, deleteItem}) {
 
-
   // populates data into a viewable list in HTML
-  const todosList = item.map((items) => {  
-    return <Todo key = {items.id} item={items} completed={completed} deleteItem={deleteItem} />
+  const todosList = item.map((item) => {  
+    return <Todo key = {item.id} item={item} completed={completed} deleteItem={deleteItem} />
   });
   return (
     <>
